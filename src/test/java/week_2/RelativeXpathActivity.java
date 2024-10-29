4cd026465ca8c1a598b9aaf76338f5734333c6b6
mitrasadat <sadat.meet@gmail.com>
2024-10-28 20:23:04 -0400

    relativeXpath activity

---
+++ src/test/java/week_2/RelativeXpathActivity.java
@@ -0,0 +1,35 @@
+package week_2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RelativeXpathActivity {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://retail.tekschool-students.com/");
+
+        WebElement search = driver.findElement(By.id("searchInput"));
+        search.sendKeys("keyboard");
+
+        driver.findElement(By.id("searchBtn")).click();
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//img[@alt='NPET K10 Gaming Keyboard']")).click();
+
+        Thread.sleep(1000);
+
+        WebElement titleElements = driver.findElement(By.xpath("//h1[@class='product__name']"));
+        System.out.println(titleElements.getText());
+
+        Thread.sleep(3000);
+
+        driver.quit();
+
+    }
+}
