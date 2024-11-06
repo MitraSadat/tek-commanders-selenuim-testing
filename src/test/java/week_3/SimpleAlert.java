7c690d881e1e63fb61aa43066471a93f8623751c
mitrasadat <sadat.meet@gmail.com>
2024-11-05 20:09:22 -0500

    working with alert

---
+++ src/test/java/week_3/SimpleAlert.java
@@ -0,0 +1,36 @@
+package week_3;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleAlert {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        String url = "https://retail.tekschool-students.com/selenium/javascript-alerts";
+
+        driver.get(url);
+        driver.manage().window().maximize();
+
+        WebElement alertBtn = driver.findElement(By.id("confirmedBtn"));
+        alertBtn.click();
+
+        Thread.sleep(2000);
+
+        Alert simpleAlert = driver.switchTo().alert();
+
+        System.out.println(simpleAlert.getText());
+
+        simpleAlert.accept();
+
+        driver.quit();
+
+
+
+
+    }
+}
